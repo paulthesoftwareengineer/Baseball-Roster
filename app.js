ef6e2b923eacbbf0f3133ca1a47f41e9504c913c
Paul Turner <baldheader@live.com>
2023-12-26 22:36:15 -0800

    updated main page

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-const battingAverages = [];
-const homeRuns = [];
-const runsBattedIn = [];
-const stolenBases = [];
-const playerNames = [];
+const teamBattingAverages = [];
+const teamHomeRuns = [];
+const teamRunsBattedIn = [];
+const teamStolenBases = [];
+const teamNames = [];
 
-const ctx = document.getElementById('playerStatsChart').getContext('2d');
+const ctx = document.getElementById('teamStatsChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: playerNames,
+        labels: teamNames,
         datasets: [
             {
-                label: 'Batting Average',
-                data: battingAverages,
+                label: 'Team Batting Average',
+                data: teamBattingAverages,
                 backgroundColor: 'rgba(54, 162, 235, 0.2)',
                 borderColor: 'rgba(54, 162, 235, 1)',
                 borderWidth: 1
             },
             {
-                label: 'Home Runs',
-                data: homeRuns,
+                label: 'Team Home Runs',
+                data: teamHomeRuns,
                 backgroundColor: 'rgba(255, 99, 132, 0.2)',
                 borderColor: 'rgba(255, 99, 132, 1)',
                 borderWidth: 1
             },
             {
-                label: 'Runs Batted In',
-                data: runsBattedIn,
+                label: 'Team Runs Batted In',
+                data: teamRunsBattedIn,
                 backgroundColor: 'rgba(255, 206, 86, 0.2)',
                 borderColor: 'rgba(255, 206, 86, 1)',
                 borderWidth: 1
             },
             {
-                label: 'Stolen Bases',
-                data: stolenBases,
+                label: 'Team Stolen Bases',
+                data: teamStolenBases,
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 borderColor: 'rgba(75, 192, 192, 1)',
                 borderWidth: 1
@@ -49,30 +49,29 @@ const myChart = new Chart(ctx, {
     }
 });
 
+const teamRows = document.querySelectorAll('tbody tr');
 
-const rows = document.querySelectorAll('tbody tr');
+teamRows.forEach(row => {
+    const teamName = row.children[0].innerText;
+    const battingAverage = parseFloat(row.children[1].innerText);
+    const hr = parseInt(row.children[2].innerText);
+    const rbi = parseInt(row.children[3].innerText);
+    const sb = parseInt(row.children[4].innerText);
 
-rows.forEach(row => {
-    const playerName = row.children[1].innerText;
-    const battingAverage = row.children[7].innerText;
-    const hr = row.children[8].innerText;
-    const rbi = row.children[9].innerText;
-    const sb = row.children[10].innerText;
-
-    if (battingAverage !== '-') {
-        playerNames.push(playerName);
-        battingAverages.push(parseFloat(battingAverage.slice(1)));
+    if (!isNaN(battingAverage)) {
+        teamNames.push(teamName);
+        teamBattingAverages.push(battingAverage);
     }
 
-    if (hr !== '-') {
-        homeRuns.push(parseInt(hr));
+    if (!isNaN(hr)) {
+        teamHomeRuns.push(hr);
     }
 
-    if (rbi !== '-') {
-        runsBattedIn.push(parseInt(rbi));
+    if (!isNaN(rbi)) {
+        teamRunsBattedIn.push(rbi);
     }
 
-    if (sb !== '-') {
-        stolenBases.push(parseInt(sb));
+    if (!isNaN(sb)) {
+        teamStolenBases.push(sb);
     }
 });
